d9583a6bbb35c192f7df199b147145b6c07260b1
Wilbur <Wenbin.Chen@Oracle.com>
2023-06-07 17:35:49 +0800

    第一次不执行EBR动作

--- codes/deploy/ebr.sh
+++ codes/deploy/ebr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "apply ebr sql file"
-sql inventoryUser/oracle@labs.local:1522/ORCLPDB1 @dbv2.sql
+# sql inventoryUser/oracle@labs.local:1522/ORCLPDB1 @dbv2.sql
 echo "apply ebr sql file successfully"
